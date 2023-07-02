8252eaffe07ce2535601b6bd2bb72f521759507f
WellKoopa / HabboCMS <44746399+wellkoopa@users.noreply.github.com>
2023-07-02 21:59:24 +0200

    Delete templates directory

--- templates/Footer.php
+++
@@ -1,7 +0,0 @@
-<div class="box" style="margin-left: 5px;margin-right: 5px;clear: both;margin-bottom: 10px;">
-    <div style="padding: 10px;">
-        © <?= date('Y', time()); ?> - <?= APP_NAME; ?>.<br />
-        <?= APP_NAME; ?> est un projet indépendant, à but non lucratif.<br />
-        Nous ne sommes pas approuvé, affiliés, ou offertes par Sulake Corporation LTD.<br />
-    </div>
-</div>
